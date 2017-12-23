ee4a38ec6cbe243bda4de5871d15bded31491d79
HaoZeke <rohit.goswami@aol.com>
2017-12-24 04:47:00 +0530

    Update CI stuff

--- scripts/ciBuild.sh
+++ scripts/ciBuild.sh
@@ -2,9 +2,12 @@
 
 set -e # halt script on error
 
+# Global
+yarn global add docusaurus-init
+
 # Build the damn thing
 cd website
-yarn global add docusaurus-init
+yarn
 yarn build
 cp ../keybase.txt build/superYodaDocs/keybase.txt
 cd ../
