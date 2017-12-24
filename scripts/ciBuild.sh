307196d5a46e1bc5e76e5103fe4fb59f81e92c60
HaoZeke <rohit.goswami@aol.com>
2017-12-24 05:36:36 +0530

    Switch to a leaner implementation of keybase proofs

--- scripts/ciBuild.sh
+++ scripts/ciBuild.sh
@@ -2,12 +2,7 @@
 
 set -e # halt script on error
 
-# Global
-yarn global add docusaurus-init
-
 # Build the damn thing
 cd website
 yarn
 yarn build
-cp ../keybase.txt build/superYodaDocs/keybase.txt
-cd ../
